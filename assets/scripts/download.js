b2928d5af6ed3d5a44f3f81e9c25864fecbda450
Guillaume Gorvel <ggorvel@gmail.com>
2026-05-05 00:55:56 +0200

    feat: add download.js

--- assets/scripts/download.js
+++ assets/scripts/download.js
@@ -1,1 +1,15 @@
-console.log('Sidebar JS chargé');
+const form = document.querySelector('.form-download');
+const fileInput = document.getElementById('plant_upload_photo');
+const fileName = document.querySelector('.file-name');
+
+if (form && fileInput) {
+    fileInput.addEventListener('change', () => {
+        if (fileName && fileInput.files.length > 0) {
+            fileName.textContent = fileInput.files[0].name;
+        }
+
+        if (fileInput.files.length > 0) {
+            form.requestSubmit();
+        }
+    });
+}
